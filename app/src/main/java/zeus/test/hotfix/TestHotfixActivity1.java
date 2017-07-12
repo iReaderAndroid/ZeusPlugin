281404c867f84c155802bca58beb6bd1501ec9a8
jimor1234567 <jimor1234567@sina.com>
2017-07-12 11:26:01 +0800

    添加漏掉的文件

---
+++ app/src/main/java/zeus/test/hotfix/TestHotfixActivity1.java
@@ -0,0 +1,7 @@
+package zeus.test.hotfix;
+
+/**
+ * Created by jimor on 2017/3/15.
+ */
+public class TestHotfixActivity1 extends TestHotFixActivity{
+}
---
+++ app/src/main/java/zeus/test/hotfixTest/MyInterface.java
@@ -0,0 +1,9 @@
+package zeus.test.hotfixTest;
+
+/**
+ * Created by jimor on 2017/3/15.
+ */
+public interface MyInterface {
+
+    String getString();
+}
